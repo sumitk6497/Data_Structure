08d65070d71f84af34dfccb769ce79c4c0f40e9d
Sumit Kumar <sumitkn.pine@gmail.com>
2021-03-02 23:29:21 +0530

    Linked List Reverse (Recursive)

---
+++ LL_reverse_recursive.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node
+{
+	int data;
+	struct Node *next;
+}NODE;
+
+NODE *newNode(int data)
+{
+	NODE *new_node = NULL;
+	new_node = (NODE *)malloc(sizeof(NODE));
+	if(NULL == new_node)
+	{
+	    printf("Memory is not available...!!!!\n");
+	    exit(0);
+	}
+	else
+	{
+	    new_node->data = data;
+	    new_node->next = NULL;
+	    return new_node;
+	}
+}
+
+void displayList(NODE *head)
+{
+	NODE *temp = NULL;
+	temp = head;
+
+	if(temp == NULL)
+	{
+	    printf("List is empty.....!!!!\n");
+	}
+	else
+	{
+	    while(temp != NULL)
+	    {
+	        printf("%d\t",temp->data);
+		temp = temp->next;
+	    }
+	    printf("\n");
+	}
+}
+
+
+
+NODE *reverseLL(NODE *head)
+{
+	if(head == NULL || head->next == NULL)
+	    return head;
+
+	NODE *rest = reverseLL(head->next);
+	head->next->next = head;
+	head->next = NULL;
+
+	return rest;
+}
+
+void deleteList(NODE **head)
+{
+        NODE *current = NULL;
+        NODE *temp = NULL;
+
+        current = *head;
+
+        while(current != NULL)
+        {
+            temp = current->next;
+            free(current);
+            current = temp;
+        }
+        *head = NULL;
+}
+
+
+int main()
+{
+	NODE *head = newNode(1);
+	head->next = newNode(2);
+	head->next->next = newNode(3);
+	head->next->next->next = newNode(4);
+
+	displayList(head);
+	head = reverseLL(head);
+	displayList(head);
+
+	deleteList(&head);
+	return 0;
+}
